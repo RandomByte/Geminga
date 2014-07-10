faf343f0475c3daec19ec833fb3ef573f72eecbe
RandomByte <merlin.lab@me.com>
2014-07-10 20:00:00 +0200

    Initial GitHub commit

---
+++ app/js/app.js
@@ -0,0 +1,310 @@
+
+$(document).ready(function () {
+    $('.btn').button()
+
+    if (window.location.pathname !== "/app/login.html") {
+        this.geminga = new Geminga.App();
+    } else {
+
+        $('#password').keypress(function (oEvent) {
+          if (oEvent.which == 13) {
+            $('#loginBtn').click();
+            oEvent.preventDefault();
+          }
+        });
+
+         $('#loginBtn').click(function(oEvent) {
+            var $that = $(this);
+            $that.button('loading');
+
+            Geminga.util.API.post("login", { password: $('#password').val() },
+                function () {
+                    window.location.href = "/app/index.html";
+                },
+                function (sError) {
+                    if (sError === "Unauthorized") {
+                        this.button('unauthorized');
+                    } else {
+                        this.button('error');
+                    }
+                }, $that);
+
+         });
+    }
+});
+
+Geminga = {};
+Geminga.App = function () {
+    var oInstance,
+        oClass = function () {
+            var that = this;
+
+            this.loadResources();
+            
+            $('#resourceDetailModal').on('show.bs.modal', function (oEvent) {
+                var oResource = oEvent.relatedTarget,
+                    oBody = "";
+
+                // Title
+                $('#resourceDetailModal #resourceTitle').html(oResource.name +
+                    " <span id='status'></span>");
+                
+                that.updateSingleStatus(oResource.id, $('#resourceDetailModal #resourceTitle #status'));
+
+                for (var i = 0; i < oResource.actions.length; i++) {
+                    if (i > 0)
+                        oBody += "</br></br>"; 
+
+                    switch (oResource.actions[i]) {
+                        case "wake":
+                            oBody += "<button id='" + oResource.actions[i] + "Btn' type='button' data-loading-text='Waking up...' data-error-text='Wake error' class='btn btn-primary'>" +
+                                "<span class='glyphicon glyphicon glyphicon-off'></span> "+
+                                "Wake up" +
+                            "</button>";
+                        break;
+                        case "shutdown":
+                            oBody += "<button id='" + oResource.actions[i] + "Btn' type='button' data-loading-text='Shutting down...' data-error-text='Shutdown error' class='btn btn-primary'>" +
+                                "<span class='glyphicon glyphicon glyphicon-off'></span> "+
+                                "Shutdown" +
+                            "</button>";
+                        break;
+                        case "vpn-start":
+                            oBody += "<button id='" + oResource.actions[i] + "Btn' type='button' data-loading-text='Starting VPN...' data-error-text='VPN start error' class='btn btn-primary'>" +
+                                "<span class='glyphicon glyphicon glyphicon-arrow-up'></span> "+
+                                "Start VPN" +
+                            "</button>";
+                        break;
+                        case "vpn-stop":
+                            oBody += "<button id='" + oResource.actions[i] + "Btn' type='button' data-loading-text='Stopping VPN...' data-error-text='VPN stop error' class='btn btn-primary'>" +
+                                "<span class='glyphicon glyphicon glyphicon-arrow-down'></span> "+
+                                "Stop VPN" +
+                            "</button>";
+                        break;
+                    }
+                };
+
+                if (oResource.actions.length === 0) {
+                     oBody += "<div class='alert alert-info'>No actions configured for " + oResource.name + "</div>";
+                }
+
+                // Body
+                $('#resourceDetailModal #resourceDetail').html(oBody);
+
+                for (var j = 0; j < oResource.actions.length; j++) {
+                     $('#' + oResource.actions[j] + 'Btn').click(function() {
+                        var sAction = oResource.actions[j];
+
+                        return function (oEvent) {
+                            $(this).button("loading");
+                            Geminga.util.API.get(sAction+ "/" + oResource.id,
+                                function (oResponse) {
+                                    $(this).button("reset");
+                                },
+                                function (sError) {
+                                    if (window.console)
+                                        window.console.log(sError);
+                                    $(this).button("error");
+                                }, this);
+                        };
+                    }());
+                };
+            });
+
+        };
+
+    Geminga.App = function() {
+        if (!oInstance) {
+            oInstance = new oClass();
+        };
+        return oInstance;
+    };
+
+    oClass.prototype = {
+
+        aDynamicResourceColumns: [
+            {
+                name: "status",
+                headerContent:
+                    "Status " +
+                    "<button type='button' class='btn btn-default btn-xs' id='refreshState'>" +
+                      "<span class='glyphicon glyphicon-refresh'></span>" +
+                    "</button>",
+                postProcessing: function () {
+                    $("#resources #refreshState").click(function () {
+                        oInstance.updateStatus();
+                    });
+                }
+            }
+        ],
+        aHiddenColumns: ["id", "mac", "actions"],
+
+        loadResources: function () {
+
+            Geminga.util.API.get("resources",
+                function (oResources) {
+
+                    this.oResources = oResources;
+
+                    for (var i = 0; i < oResources.length; i++) {
+                        var oResource = oResources[i],
+                            sHeaderRow = "",
+                            sRow = "",
+                            bMacGiven;
+
+                        for (var sProp in oResource) {
+                            if (this.aHiddenColumns.indexOf(sProp) === -1) {
+                                if (i === 0) // Header
+                                    sHeaderRow += "<th id='" + sProp + "'>" + sProp + "</th>";
+
+                                sRow += "<td id='" + sProp + "'>" + oResource[sProp] + "</td>";
+                            }
+                        }
+
+                        // Append dynamic columns
+                        for (var k = 0; k < this.aDynamicResourceColumns.length; k++) {
+                            sRow += "<td id='" + this.aDynamicResourceColumns[k].name + "'></td>";
+                        };
+
+                        if (i === 0) { // Header
+                            for (var l = 0; l < this.aDynamicResourceColumns.length; l++) {
+                                sHeaderRow += "<th id'" + this.aDynamicResourceColumns[l].name + ">" +
+                                    this.aDynamicResourceColumns[l].headerContent + "</th>";
+                            };
+
+                            $("#resources").append("<tr id='header'>" + sHeaderRow + "</tr>");
+                        }
+
+                        $("#resources").append("<tr id='" + oResource.id + "'>" + sRow + "</tr>");
+                        
+                        $("#resources #" + oResource.id).click(function() {
+                            var oRes = oResource;
+                            return function () {
+                                $('#resourceDetailModal').modal('show', oRes);
+                            };
+                        }());
+                    };
+
+                    for (var m = 0; m < this.aDynamicResourceColumns.length; m++) {
+                        this.aDynamicResourceColumns[m].postProcessing();
+                    };
+
+                    this.updateStatus(); // todo event this!
+
+                },
+                function (sError) {
+                    // todo errorhandling
+                }, this);
+
+        },
+
+        updateStatus: function() {
+
+            for (var i = 0; i < this.oResources.length; i++) {
+                var oRes = this.oResources[i];
+
+                $("#" + oRes.id + " #status")
+                    .html("<img class='busyIndicator' src='img/spinner.gif'/>");
+
+                Geminga.util.API.get("status/" + oRes.id,
+                    function () {
+                        var oResource = oRes;
+                        return function (oStatus) {
+                            $("#" + oResource.id + " #status").html(
+                                oStatus.alive ? "<span class='label label-success'>Online</span>" :
+                                    "<span class='label label-default'>Offline</span>");
+                        } 
+                    }(),
+                    function (sError) {
+                        // todo
+                    }, this);
+
+            };
+        },
+
+        updateSingleStatus: function (sId, oIndicator) {
+            oIndicator.html("<img class='busyIndicator' src='img/spinner.gif'/>");
+
+                Geminga.util.API.get("status/" + sId,
+                    function (oStatus) {
+                        var sLabelType,
+                            sIndicator = "",
+                            i, oState;
+
+                        sIndicator += oStatus.alive ? "<span class='label label-success'>Online</span>" :
+                            "<span class='label label-default'>Offline</span>";
+                        sIndicator += " ";
+
+                        if (oStatus.remoteConnected === undefined) {
+                            // Isn't and shouldn't be connected
+                            sLabelType = oStatus.alive ? "info" : "default";
+                        } else if (oStatus.remoteConnected === false) {
+                            sLabelType = oStatus.alive ? "danger" : "default";
+                        } else {
+                            sLabelType = "success";
+                        }
+                        sIndicator += "<span class='label label-" + sLabelType + "'>Remote" +
+                            (oStatus.remoteConnected ? "" : " not") + " connected</span>";
+                        sIndicator += " ";
+
+                        if (oStatus.states) {
+                            for (i = oStatus.states.length - 1; i >= 0; i--) {
+                                oState = oStatus.states[i];
+                                sLabelType = oState.state === 'started' ? "success" : "default";
+                                
+                                sIndicator += "<span class='label label-" + sLabelType + "'>" +
+                                    oState.name + " is " + oState.state + "</span>";
+                                
+                                if (i > 0)
+                                    sIndicator += " ";
+                            }
+                        }
+
+                        oIndicator.html(sIndicator);
+                    },
+                    function (sError) {
+                        // todo
+                    }, this);
+        }
+
+    };
+    return Geminga.App();
+};
+
+Geminga.util = {};
+Geminga.util.API = {};
+
+Geminga.util.API.get = function (sUrl, fnOnSuccess, fnOnError, oCallee) {
+    Geminga.util.API.send('get', sUrl, undefined, fnOnSuccess, fnOnError, oCallee);
+};
+
+Geminga.util.API.post = function (sUrl, oData, fnOnSuccess, fnOnError, oCallee) {
+    Geminga.util.API.send('post', sUrl, oData, fnOnSuccess, fnOnError, oCallee);
+};
+
+Geminga.util.API.send = function (sType, sUrl, oData, fnOnSuccess, fnOnError, oCallee) {
+    jQuery.ajax({
+        type: sType,
+        url: '/api/' + sUrl,
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(oData),
+        success: function (oData) {
+            fnOnSuccess.call(oCallee || this, oData);
+        },
+        error: function (oReq, sStatus, sError) {
+            var sErrorMsg;
+
+            if (sError === "Unauthorized" && window.location.pathname !== "/app/login.html") {
+                window.location.href = "/app/login.html";
+            } else {
+                try {
+                    sErrorMsg = JSON.parse(oReq.responseText).error;
+                } catch (err) {
+                    sErrorMsg = sError;
+                }
+
+                fnOnError.call(oCallee || this, sErrorMsg, oReq, sStatus, sError);
+            }
+            
+        }
+    });
+};
---
+++ geminga.js
@@ -0,0 +1,265 @@
+var express         = require('express'),
+    app             = express(),
+    ping            = require("ping"),
+    wol             = require('wake_on_lan'),
+    net             = require('net'),
+    JSONSocket      = require('json-socket'),
+    config          = require('./config.json');
+
+if (!config || !config.port || !config.resources ||
+        !config.password || !config.cookieSecret || !config.sessionSecret) {
+    console.log("There's something missing in your config.json, please refer to config.example.json for an example");
+    process.exit(1);
+}
+
+
+app.configure(function(){
+    app.use(express.json());
+    app.use(express.cookieParser(config.cookieSecret));
+    app.use(express.cookieSession({ secret: config.sessionSecret, key: "geminga.sid",
+                cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+            }));
+    app.use('/app', express.static(__dirname + '/app'));
+
+    app.use(function(err, req, res, next){
+      console.log(err);
+      res.status(err.status || 500);
+      res.send({ error: err });
+    });
+
+    app.use(app.router);
+});
+
+app.listen(config.port);
+console.log('Geminga running on port ' + config.port);
+
+var aResources = config.resources,
+    aSessions = [],
+    aPublicResourceData = ["id", "name", "location", "actions"];
+
+app.get('/', function(req, res){
+    if (req.session && aSessions.indexOf(req.session) !== -1) {
+        res.redirect("/app/index.html");    
+    } else {
+        res.redirect("/app/login.html");
+    }
+});
+
+app.post('/api/login', function(req, res) {
+    if (req.session && aSessions.indexOf(req.session) !== -1) {
+        res.redirect("/app/index.html");
+    } else if (req.body.password === config.password) {
+        req.session = generateGuid();
+        aSessions.push(req.session);
+        res.send(200);
+    } else {
+        res.redirect("/app/login.html");
+    }
+});
+
+app.get('/api/logout', function(req, res) {
+    var iIndex;
+    if (req.session) {
+        iIndex = aSessions.indexOf(req.session);
+        if (iIndex > -1) {
+            aSessions.slice(iIndex, 1);
+            req.session = null;
+            res.redirect("/app/login.html");
+        } else {
+            req.session = null;
+            res.redirect("/app/login.html");
+        }
+    } else {
+        res.send(400);
+    }
+});
+
+// Status
+app.get('/api/resources', function(req, res){
+    if (!req.session || aSessions.indexOf(req.session) === -1) {
+        res.send(401);
+    } else {
+        var aPublicResources = [];
+
+        for (var i = 0; i < aResources.length; i++) {
+            var oInternalResource = aResources[i],
+                oPublicResource = {};
+
+            for (var j = 0; j < aPublicResourceData.length; j++) {
+                oPublicResource[aPublicResourceData[j]] = oInternalResource[aPublicResourceData[j]];
+            };
+
+            aPublicResources.push(oPublicResource);
+        };
+
+        res.send(200, aPublicResources);
+    }
+});
+
+app.get('/api/status/:resource', function(req, res){
+    if (!req.session || aSessions.indexOf(req.session) === -1) {
+        res.send(401);
+    } else {
+        for (var i = aResources.length - 1; i >= 0; i--) {
+            var oResource = aResources[i],
+                oResponse = {};
+
+            if (oResource.id === req.params.resource) {
+
+                if (oResource.token) {
+                    sendCommandToResource(oResource, "checkConnectivity",
+                        function (oRes) {
+                            res.send({ alive: true, remoteConnected: true, states: oRes.states });
+                        },
+                        function (oError) {
+                            ping.sys.probe(oResource.ip || oResource.hostname, function(bAlive) {
+                                res.send({ alive: bAlive, remoteConnected: false });
+                            }); 
+                        });
+                } else {
+                    ping.sys.probe(oResource.ip || oResource.hostname, function(bAlive) {
+                        res.send({ alive: bAlive });
+                    });    
+                }
+                return;
+            }
+        }
+    }
+});
+
+// Wake on Lan
+app.get('/api/wake/:resource', function(req, res){
+    if (!req.session || aSessions.indexOf(req.session) === -1) {
+        res.send(401);
+    } else {
+        for (var i = aResources.length - 1; i >= 0; i--) {
+            var oResource = aResources[i];
+
+            if (oResource.id === req.params.resource) {
+                if (!oResource.mac) {
+                    res.send(500, { error: "No MAC adress specified for " + oResource.name });
+                }
+                else if (oResource.actions.indexOf("wake") === -1) {
+                    res.send(400, { error: "Action not activated for " + oResource.name });
+                } else {
+                    wol.wake(oResource.mac, function(oError) {
+                        if (oError) {
+                            res.send(500, { error: oError });
+                        } else {
+                            res.send(200);
+                        }
+                    });    
+                }
+                return;
+            }
+        }
+    }
+});
+
+// Shutdown
+app.get('/api/shutdown/:resource', function(req, res){
+    if (!req.session || aSessions.indexOf(req.session) === -1) {
+        res.send(401);
+    } else {
+        for (var i = aResources.length - 1; i >= 0; i--) {
+            var oResource = aResources[i];
+
+            if (oResource.id === req.params.resource) {
+                if (oResource.actions.indexOf("shutdown") === -1) {
+                    res.send(400, { error: "Action not activated for " + oResource.name });
+                } else {
+                    sendCommandToResource(oResource, "shutdown",
+                        function (oRes) {
+                            res.send(200);
+                        },
+                        function (oError) {
+                            res.send(500, { error: oError });
+                        });
+                }
+                return;
+            }
+        }
+    }
+});
+
+/*** VPN ***/
+app.get('/api/vpn-start/:resource', function(req, res){
+    if (!req.session || aSessions.indexOf(req.session) === -1) {
+        res.send(401);
+    } else {
+        for (var i = aResources.length - 1; i >= 0; i--) {
+            var oResource = aResources[i];
+
+            if (oResource.id === req.params.resource) {
+                if (oResource.actions.indexOf("vpn-start") === -1) {
+                    res.send(400, { error: "Action not activated for " + oResource.name });
+                } else {
+                    sendCommandToResource(oResource, "vpn-start",
+                        function (oRes) {
+                            res.send(200);
+                        },
+                        function (oError) {
+                            res.send(500, { error: oError });
+                        });
+                }
+                return;
+            }
+        }
+    }
+});
+
+app.get('/api/vpn-stop/:resource', function(req, res){
+    if (!req.session || aSessions.indexOf(req.session) === -1) {
+        res.send(401);
+    } else {
+        for (var i = aResources.length - 1; i >= 0; i--) {
+            var oResource = aResources[i];
+
+            if (oResource.id === req.params.resource) {
+                if (oResource.actions.indexOf("vpn-stop") === -1) {
+                    res.send(400, { error: "Action not activated for " + oResource.name });
+                } else {
+                    sendCommandToResource(oResource, "vpn-stop",
+                        function (oRes) {
+                            res.send(200);
+                        },
+                        function (oError) {
+                            res.send(500, { error: oError });
+                        });
+                }
+                return;
+            }
+        }
+    }
+});
+
+function sendCommandToResource (oResource, sCommand, onSuccess, onError) {
+    var socket = new JSONSocket(new net.Socket());
+
+    socket.connect(40404, oResource.ip || oResource.hostname);
+   
+    socket.on('connect', function() { //Don't send until we're connected
+        socket.sendMessage({ command: sCommand, token: oResource.token});
+    });
+    socket.on('message', function(oRes) {
+        if (oRes.error) {
+            onError(oRes.error);
+        } else {
+            onSuccess(oRes);   
+        }
+    });
+    socket.on('error', function(oError) {
+        onError(oError);
+    });
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+             .toString(16)
+             .substring(1);
+};
+
+function generateGuid() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();
+}
